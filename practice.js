e0dc779c68057974bc11228181a111546d7932d1
ScottLeeBJJ <scottlee5455@outlook.com>
2022-04-25 00:37:17 -0700

    practice update

--- practice.js
+++ practice.js
@@ -36,3 +36,6 @@ var myVar = 0;
 // You can also declare a variable like this:
 
 var myName = "your name";
+
+// "your name" is called a string literal. A string literal, or string, is a series of zero or more characters enclosed in single or double quotes.
+
